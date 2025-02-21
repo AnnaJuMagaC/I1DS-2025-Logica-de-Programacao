c42ce340d2b0c2066fe26a93a7ba8f49a8c99cb8
AnnaJuMagaC <anna.j.costa8@alunosenai.br>
2025-02-21 17:08:04 -0300

    correção caraCoroa

--- Atividades/CaraCoroa.js
+++ Atividades/CaraCoroa.js
@@ -1,17 +1,12 @@
-//Jogo cara e coroa - melhor de três rodadas - aleatório - DO-WHILE
-//Apresentar o placar das rodadas ex:
-var cara = 0
-var coroa= 0
-var conatdor = 1
+// Jogo cara e coroa - melhor de três rodadas - aleatório - DO-WHILE
+//apresentar um placar das rodadas ex:
+var cara = 0;
+var coroa = 0;
+var contador = 1;
 
-do {let l1 = Math.random * 50;
-  console.log()
-}
-
-
-console.log("------------------------------")
-console.log("           Placar             ")
-console.log("------------------------------")
-console.log(`Vitórias: 'variavelVitoria`)
-console.log(`Derrotas: 'variavelDerrotas`)
-console.log("------------------------------")
+console.log(`-------------------------------------------------`);
+console.log(`------------------------PLACAR-------------------`);
+console.log(`-------------------------------------------------`);
+console.log(`Vitórias: 'variavelVitoria`);
+console.log(`Derrotas: 'variavelDerrotas`);
+console.log(`-------------------------------------------------`);

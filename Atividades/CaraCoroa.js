d0107d6415f750f8fcf69861fe88dd4ad51552ce
AnnaJuMagaC <anna.j.costa8@alunosenai.br>
2025-02-27 08:19:10 -0300

    correção professor caraCoroa

--- Atividades/CaraCoroa.js
+++ Atividades/CaraCoroa.js
@@ -2,11 +2,17 @@
 //apresentar um placar das rodadas ex:
 var cara = 0;
 var coroa = 0;
-var contador = 1;
+var i = 1;
 
-console.log(`-------------------------------------------------`);
-console.log(`------------------------PLACAR-------------------`);
-console.log(`-------------------------------------------------`);
-console.log(`Vitórias: 'variavelVitoria`);
-console.log(`Derrotas: 'variavelDerrotas`);
-console.log(`-------------------------------------------------`);
+do {
+  let face = Math.random() * 100;
+  face < 50 ? cara++ : coroa++;
+  i++;
+} while (i <= 3);
+
+console.log(`--------------------------------------------`);
+console.log(`-------------------PLACAR-------------------`);
+console.log(`--------------------------------------------`);
+console.log(`Cara: ${cara}`);
+console.log(`Coroa: ${coroa}`);
+console.log(`--------------------------------------------`);
